93e0bb71ac841b7d304b81c0020cf934575298c2
BRITNEY.姜二丫 <9908104+jy627627@user.noreply.gitee.com>
2023-05-10 17:39:20 +0800

    first commit

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React from "react";
+
+interface menuItemProps {
+    onClick: () => void;
+    label: string
+}
+
+export const MenuItem:React.FC<menuItemProps> = ({
+    onClick,
+    label
+}) => {
+    return (
+        <div
+            onClick={onClick}
+            className="
+                px-4
+                py-3
+                hover:bg-neutral-100
+                transition
+                font-semibold
+            "
+        >
+            {label}
+        </div>
+    )
+}
